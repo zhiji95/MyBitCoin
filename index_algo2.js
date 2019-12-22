342c8c8d01df89ec10dafc14c4538870f423576f
Zhi Ji <33732707+zhiji95@users.noreply.github.com>
2019-12-22 17:08:00 -0500

    finish

--- index_algo2.js
+++ index_algo2.js
@@ -88,7 +88,7 @@ function createUser() {
                 
                     )   
                     const txSigned = BigchainDB.Transaction.signTransaction(tx, user.privateKey)
-                    conn.postTransactionCommit(txSigned).then(()=>{location.reload();})
+                    conn.postTransactionCommit(txSigned)
         
             } 
     }, 1000);
@@ -173,28 +173,11 @@ function addFund() {
                             
                                 )   
                                 const txSigned = BigchainDB.Transaction.signTransaction(tx, keyPairs[i].privateKey)
-                                console.log(txSigned)
-                                conn.postTransactionCommit(txSigned) 
+                                conn.postTransactionCommit(txSigned)
+                            
+                            
                         });
-                } else {
-                    const tx = BigchainDB.Transaction.makeCreateTransaction(
-                        // Define the asset to store, in this example it is the current temperature
-                        // (in Celsius) for the city of Berlin.
-                        { value: "1 share"},
-                    
-                        // Metadata contains information about the transaction itself
-                        // (can be `null` if not needed)
-                        { datetime: new Date().toString() },
-                    
-                        // A transaction needs an output
-                        [ BigchainDB.Transaction.makeOutput(
-                                BigchainDB.Transaction.makeEd25519Condition(keyPairs[i].publicKey), amount)
-                        ],
-                        keyPairs[i].publicKey
-                        )   
-                        const txSigned = BigchainDB.Transaction.signTransaction(tx, keyPairs[i].privateKey)
-                        console.log(txSigned)
-                        conn.postTransactionCommit(txSigned).then(()=>{location.reload();})
+
                 } 
             })
     } ,1000)
@@ -204,10 +187,6 @@ function addFund() {
 function transfer() {
     const sender = document.getElementById("sender").value;
     const receiver = document.getElementById("receiver").value;
-    if (sender == receiver) {
-        alert("Illegal transfer detected");
-        return
-    }
     const amount = parseInt(document.getElementById("amount_receiver").value);
     let keyPairs = [];
     let usernames = [];
@@ -265,45 +244,20 @@ function transfer() {
                                                     alert("Sender do not have sufficient fund.");
                                                 } else {
                                                     const senderNewAmount = senderAmount - amount;
-                                                    const receiverNewAmount = receiverAmount + amount;
-                                                    const tx = BigchainDB.Transaction.makeCreateTransaction(
-                                                        // Define the asset to store, in this example it is the current temperature
-                                                        // (in Celsius) for the city of Berlin.
-                                                        { value: "1 share"},
-                                                    
-                                                        // Metadata contains information about the transaction itself
-                                                        // (can be `null` if not needed)
-                                                        { datetime: new Date().toString() },
-                                                    
-                                                        // A transaction needs an output
-                                                        [ BigchainDB.Transaction.makeOutput(
-                                                                BigchainDB.Transaction.makeEd25519Condition(senderKey.publicKey), senderNewAmount.toString())
+                                                    const receiverNewAmount = receiverAmount - amount;
+                                                    const txTransfer = BigchainDB.Transaction.makeTransferTransaction(
+                                                        // signedTx to transfer and output index
+                                                        [{ tx: senderTransaction, output_index: 0 }, { tx: receiverTransaction, output_index: 0 }],
+                                                
+                                                        [BigchainDB.Transaction.makeOutput(BigchainDB.Transaction.makeEd25519Condition(senderKey.publicKey), senderNewAmount.toString()),
+                                                        BigchainDB.Transaction.makeOutput(BigchainDB.Transaction.makeEd25519Condition(receiverKey.publicKey), receiverNewAmount.toString())
                                                         ],
-                                                        senderKey.publicKey
-                                                        )   
-                                                        const txSigned = BigchainDB.Transaction.signTransaction(tx, senderKey.privateKey)
-                                                        conn.postTransactionCommit(txSigned).then(
-                                                            () => {
-                                                                const tx = BigchainDB.Transaction.makeCreateTransaction(
-                                                                    // Define the asset to store, in this example it is the current temperature
-                                                                    // (in Celsius) for the city of Berlin.
-                                                                    { value: "1 share"},
-                                                                
-                                                                    // Metadata contains information about the transaction itself
-                                                                    // (can be `null` if not needed)
-                                                                    { datetime: new Date().toString() },
-                                                                
-                                                                    // A transaction needs an output
-                                                                    [ BigchainDB.Transaction.makeOutput(
-                                                                            BigchainDB.Transaction.makeEd25519Condition(receiverKey.publicKey), receiverNewAmount.toString())
-                                                                    ],
-                                                                    receiverKey.publicKey
-                                                                    )   
-                                                                    const txSigned = BigchainDB.Transaction.signTransaction(tx, receiverKey.privateKey)
-                                                                    conn.postTransactionCommit(txSigned).then(()=>{location.reload();})
-                                                            })
-                                                    // const txTransferSigned = BigchainDB.Transaction.signTransaction(txTransfer, senderKey.privateKey, receiverKey.privateKey);
-                                                    // conn.postTransactionCommit(txTransferSigned)
+                                                
+                                                        // metadata
+                                                        {type: 'Transfer', datetime: new Date().toString()}
+                                                    );
+                                                    const txTransferSigned = BigchainDB.Transaction.signTransaction(txTransfer, senderKey.privateKey, receiverKey.privateKey);
+                                                    conn.postTransactionCommit(txTransferSigned)
                                                 }
 
                                         })
@@ -347,72 +301,31 @@ function render() {
                 conn.getTransaction(transactionIds[i]).then(
                     transaction => {
                         let data = transaction["asset"]["data"];
-                        console.log(transaction)
                         // kayPairs.push(data["kayPair"]);
                         // usernames.push(data["username"]);
                         const username = data["username"];
-                        const keyPair = data["keyPair"];
-                        let html = ''
-                        if (i %2 == 0) {
-                            html += '<div class="row">'
-                        }
-                        html += '<div class="col"><div class="card" style="width: 18rem;"><img src="./icon.png" class="card-img-top" alt=" "><div class="card-body"><h5 class="User">'
-                        html += '<p>Username: '+data["username"]+'</p>';
-                        conn.listOutputs(keyPair.publicKey, false)
-                        .then(listUnspentOutputs => {
-                            if (listUnspentOutputs.length > 1) {
-                                const transactionId = listUnspentOutputs[listUnspentOutputs.length-1]["transaction_id"];
-                                conn.getTransaction(transactionId).then(
-                                    transaction => {
-                                        const amount = parseInt(transaction["outputs"][0]["amount"]);
-                                        // html += '<div class="col"><div class="card" style="width: 18rem;"><img src="./icon.png" class="card-img-top" alt=" "><div class="card-body"><h5 class="User">'
-                                        // html += '<p>Username: '+data["username"]+'</p>';
-                                        html += '<p>Balance: '+amount+'</p></div>'
-                                        html += '<a href="#" class="btn btn-primary">Profile</a></div></div></div>'
-                                        if (i %2 == 0) {
-                                            html += '</div>'
-                                        } 
-                                        $("#users").append(html)
-                                        // $("#users").append()
-                                        // $("#users").append('<p>Username: '+data["username"]+'</p>')
-                                        // $("#users").append('<p>Balance: '+amount+'</p></div>')
-                                        // $("#users").append('<a href="#" class="btn btn-primary">Profile</a></div></div>')
-                                    })
-                            } else {
-                                
-                                html += '<p>Balance: 0</p></div>'
-                                html += '<a href="#" class="btn btn-primary">Profile</a></div></div></div>'
-                                if (i %2 == 0) {
-                                    html += '</div>'
-                                } 
-                                $("#users").append(html)
-                                // $("#users").append('<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="..."><div class="card-body"><h5 class="User">')
-                                // $("#users").append('<p>Username: '+data["username"]+'</p>')
-                                // $("#users").append('<p>Balance: 0</p></div>')
-                                // $("#users").append('<a href="#" class="btn btn-primary">Profile</a></div></div>')
-                            }   
-                    });
-            })
+                        const keyPairs = data["keyPair"];
+                        $("#users").append('<p>'+data["username"]+'</p>')
+                    }
+                );
+                
+                
+            }
         }
-    }})
-    let printHeight = 1;
+    })
+    
     while (true) {
         block = JSON.parse(httpGet('http://localhost:9984/api/v1/blocks/'+height));
         if (block["status"] == "404") {
             break;
         }
-        
-        if (block.transactions.length>0) {
-            addBlock(block, printHeight);
-            printHeight++;
-        }
-        
+        addBlock(block);
         height++;
     }
 
 }
 
-function addBlock(block, printHeight) {
+function addBlock(block) {
     // let blockChain = document.getElementById("test");
     // let blockChain = document.getElementsByClassName("list-group");
     const transactions = block["transactions"];
@@ -452,17 +365,14 @@ function addBlock(block, printHeight) {
                 // JSON.stringify(
                 // html += 'data:'+JSON.stringify(transaction["asset"]["data"]);
         }
-        if (printHeight % 2 == 0) {
-            $("#blockChain").append('<span class="d-block p-2 bg-primary text-white"><h5>Block Height:'+printHeight+'</h5>'+
+        $("#blockChain").append('<li class="list-group-item"><h5>Block Height:'+block["height"]+'</h5>'+
         html+
-        '</span>'); 
+        // '<h5>time:'+tr["metadata"]["datetime"]+'</h5>'+
+        '</li>'
+        );
         } else {
-            $("#blockChain").append('<span class="d-block p-2 bg-dark text-white"><h5>Block Height:'+printHeight+'</h5>'+
-        html+
-        '</span>'); 
+            $("#blockChain").append('<li class="list-group-item"><h5>Block Height:'+block["height"]+'</h5></li>');
         }
-    }
-         
     });
     
     
